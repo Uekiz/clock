bcf4a8163d2c9e0f24e178ebb32b01c27ebc847f
Uekiz <kingghostdragon@gmail.com>
2021-07-05 19:58:40 +0700

    2

--- bin/clock.dart
+++ bin/clock.dart
@@ -1,6 +1,8 @@
+import 'clockState.dart';
+
 const String input = 'on 18 0 set inc set inc set inc inc set';
 void main(List<String> arguments) {
   var splitedInput = input.split(' ');
-  print(splitedInput);
-  print(splitedInput.length);
+  var state = ClockState();
+  
 }
--- bin/clockState.dart
+++ bin/clockState.dart
@@ -7,5 +7,19 @@ class ClockState {
 
   ClockState._internal();
 
-  
+  var current = 'on';
+
+  void goNext() {
+    if(current == 'on'){
+      
+    }
+
+    if(current ==  'idle'){
+      current = 'setHr';
+    }else if(current == 'setHr'){
+      current =  'setMin';
+    }else if(current == 'setMin'){
+      current = 'idle';
+    }
+  }
 }
